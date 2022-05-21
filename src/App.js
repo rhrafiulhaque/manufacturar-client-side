aa83f67c3f09e726d58e37d961af920f6d73d720
rhrafiulhaque <rh.rafiul.haque@gmail.com>
2022-05-21 23:55:54 +0600

    router updated

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 import { ToastContainer } from 'react-toastify';
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="max-w-7xl mx-auto px-12">
       <Navbar></Navbar>
       <Routes>
-        
+        <Route></Route>
       </Routes>
       <Home></Home>
       <Footer></Footer>
